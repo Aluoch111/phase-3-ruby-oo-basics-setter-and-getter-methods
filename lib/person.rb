8125670c418d501e18a518d93173a091c6d71563
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-10-24 12:59:46 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # Setter method
+    def name= (name)
+        @name = name
+    end
 
+    # Getter method
+    def name
+        @name
+    end
+
+    def job= (job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
